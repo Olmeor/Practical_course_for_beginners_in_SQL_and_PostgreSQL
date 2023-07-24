d4d3f9cb744daffdab68b7d6916fba8618f963e4
Mikhail Oleynikov <106018937+Olmeor@users.noreply.github.com>
2023-07-24 19:10:28 +0300

    feat: add 1.5 solutions

---
+++ 01 Введение в SQL/05-Отношение многие ко многим.sql
@@ -0,0 +1,47 @@
+DROP TABLE IF EXISTS book;
+DROP TABLE IF EXISTS author;
+
+CREATE TABLE book
+(
+	book_id int PRIMARY KEY,
+	title text NOT NULL,
+	isbn varchar(32) NOT NULL
+);
+
+CREATE TABLE author
+(
+	author_id int PRIMARY KEY,
+	full_name text NOT NULL,
+	rating real
+);
+
+CREATE TABLE book_author
+(
+	book_id int REFERENCES book (book_id),
+	author_id int REFERENCES author (author_id),
+	CONSTRAINT book_author_pkey PRIMARY KEY (book_id, author_id)
+);
+
+INSERT INTO book
+VALUES
+(1, 'The Diary of a Young Girl', '0199535566'),
+(2, 'Pride and Prejudice', '9780307594006'),
+(3, 'To Kill a Mockingbird', '0446310786'),
+(4, 'The Book of Gutsy Women: Favorite Stories of Courage and Resilience', '1501178415'),
+(5, 'War and Peace', '1788886526');
+
+INSERT INTO author
+VALUES
+(1, 'Bob', 4.5),
+(2, 'Alice', 4.0),
+(3, 'John', 4.7);
+
+INSERT INTO book_author
+VALUES
+(1, 1),
+(2, 1),
+(3, 1),
+(3, 2),
+(4, 1),
+(4, 2),
+(4, 3);
