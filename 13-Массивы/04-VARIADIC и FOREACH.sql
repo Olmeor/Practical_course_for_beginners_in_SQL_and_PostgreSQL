342dbc15608b5b3ecb8ea15b9a5c9b5a65b658c2
Mikhail Oleynikov <106018937+Olmeor@users.noreply.github.com>
2023-09-03 22:08:02 +0700

    feat: add 13.4 solutions

---
+++ 13-Массивы/04-VARIADIC и FOREACH.sql
@@ -0,0 +1,31 @@
+--Чтобы передавать n-параметров (одного типа) в функцию, нужно объявить аргумент как VARIADIC:
+VARIADIC arg_name data_type[]
+
+--Пример использования VARIADIC
+
+CREATE FUNCTION filter_even(VARIADIC numbers int[]) RETURNS SETOF int AS $$
+BEGIN
+	FOR counter IN 1..array_upper(numbers, 1)
+	LOOP
+		CONTINUE WHEN counter % 2 != 0;
+		RETURN NEXT counter;
+	END LOOP;
+END;
+$$ LANGUAGE plpgsql;
+
+SELECT * FROM filter_even(1, 2, 3, 4, 5, 6, 7, 8); -- Вернет 2, 4, 6, 8
+
+--Пример использования FOREACH
+CREATE FUNCTION filter_even(VARIADIC numbers int[]) RETURNS SETOF int AS $$
+DECLARE
+    counter int;
+BEGIN
+	FOREACH counter IN ARRAY numbers
+	LOOP
+		CONTINUE WHEN counter % 2 != 0;
+		RETURN NEXT counter;
+END LOOP;
+END;
+$$ LANGUAGE plpgsql;
+
+SELECT * FROM filter_even(1, 2, 3, 4, 5, 6, 7, 8); -- Вернет 2, 4, 6, 8
